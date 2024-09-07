7b769e2026838e21850bcc2f9eddcd314357ba63
SumantSherki <sumantsherki21@gmail.com>
2024-09-07 09:13:35 +0530

    Time: 1 ms (97.88%), Space: 44.9 MB (68.61%) - LeetHub

---
+++ 1367-linked-list-in-binary-tree/1367-linked-list-in-binary-tree.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public boolean isSubPath(ListNode head, TreeNode root) {
+        if (root == null) return false;
+        return (
+            dfs(root, head) ||
+            isSubPath(head, root.left) ||
+            isSubPath(head, root.right)
+        );
+    }
+
+    private boolean dfs(TreeNode node, ListNode head) {
+        if (head == null) return true; // All nodes in the list matched
+        if (node == null) return false; // Reached end of tree without matching all nodes
+        if (node.val != head.val) return false; // Value mismatch
+        // Check both left and right children
+        return dfs(node.left, head.next) || dfs(node.right, head.next);
+    }
+}
